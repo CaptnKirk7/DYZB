ba7a96772a17a929a2f30084a378ca6fff60789a
Red <redcyan@163.com>
2019-06-04 16:16:05 +0800

    First commit from my mac to GitHub

---
+++ DYZB/DYZB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DYZB
+//
+//  Created by 宋可为 on 2019/6/4.
+//  Copyright © 2019 redcyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
